cdfcf8c60f0429b9d362d7343153e6d0153448c9
chrischep <christine.chepleting@student.moringaschool.com>
2022-10-25 08:03:51 +0300

    lab completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
 # Make your shoe class here!
+class shoe
+   
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+
+    def initialize(
+        brand = "Adidas",
+        color = "red",
+        size = 9.5,
+        material = "suede",
+        condition = "tattered")
+
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+    end
+end
